751de813be5f574e0386cd24560f171ce676756c
juli0o79 <juli0o79@hotmail.com>
2026-05-18 18:59:27 -0300

    refactoring date component

--- src/DateCounter.js
+++ src/DateCounter.js
@@ -1,34 +1,57 @@
-import { useState } from "react";
+import { useReducer } from "react";
+
+class DateState {
+  constructor() {
+    this.count = 0;
+    this.step = 1;
+  }
+};
+
+const dateStateReducer = (state, action) => {
+  const { type: actionType, payload } = action;
+  switch (actionType) {
+    case "INCREMENT_COUNT":
+      return { ...state, count: state.count + state.step };
+    case "DECREMENT_COUNT":
+      return { ...state, count: state.count - state.step };
+    case "DEFINE_COUNT":
+      return { ...state, count: payload };
+    case "DEFINE_STEP":
+      return { ...state, step: payload };
+    case "RESET":
+      return { count: 0, step: 1 }
+    default:
+      return state;
+  }
+}
 
 function DateCounter() {
-  const [count, setCount] = useState(0);
-  const [step, setStep] = useState(1);
+  const [dateState, dispatch] = useReducer(dateStateReducer, new DateState())
+
+  const { count, step } = dateState;
 
   // This mutates the date object.
   const date = new Date("june 21 2027");
   date.setDate(date.getDate() + count);
 
   const dec = function () {
-    // setCount((count) => count - 1);
-    setCount((count) => count - step);
+    dispatch({ type: "DECREMENT_COUNT" })
   };
 
   const inc = function () {
-    // setCount((count) => count + 1);
-    setCount((count) => count + step);
+    dispatch({ type: "INCREMENT_COUNT" })
   };
 
   const defineCount = function (e) {
-    setCount(Number(e.target.value));
+    dispatch({ type: "DEFINE_COUNT", payload: Number(e.target.value) })
   };
 
   const defineStep = function (e) {
-    setStep(Number(e.target.value));
+    dispatch({ type: "DEFINE_STEP", payload: Number(e.target.value) });
   };
 
   const reset = function () {
-    setCount(0);
-    setStep(1);
+    dispatch({ type: "RESET" });
   };
 
   return (
