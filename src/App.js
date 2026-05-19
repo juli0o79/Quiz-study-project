6753f60f139cd52aad0edb470624fadc6d10bb56
juli0o79 <juli0o79@hotmail.com>
2026-05-18 21:38:00 -0300

    fetching the questions data form the json server rest api and initializing reducer

--- src/App.js
+++ src/App.js
@@ -1,8 +1,37 @@
+import { useEffect, useReducer } from 'react';
 import DateCounter from './components/DateCounter';
 import Header from './components/Header';
 import Main from './components/Main';
 
+const initialState = {
+  quentions: [],
+  // loading, error, ready, active, finished
+  staus: 'loading'
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case 'dataReceived':
+      return {
+        ...state,
+        status: 'ready',
+        questions: action.payload
+      }
+    case 'dataFailed':
+      return { ...state, status: 'error' }
+    default:
+      throw new Error('Action unkown')
+  }
+}
+
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialState)
+  useEffect(() => {
+    fetch('http://localhost:9000/questions')
+      .then((res) => res.json())
+      .then(data => dispatch({ type: 'dataReceived', payload: data }))
+      .catch(err => dispatch('dataFailed'))
+  }, [])
   return <div className='app'>
     <Header />
     <Main>
