60166655a8b754b605959035633ff801aef26b6e
juli0o79 <juli0o79@hotmail.com>
2026-05-18 21:26:05 -0300

    adding the questions json, new component folders and Main component file. Installing the json server to simulate rest api

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
-import DateCounter from './DateCounter';
+import DateCounter from './components/DateCounter';
+import Header from './components/Header';
+import Main from './components/Main';
 
 function App() {
-  return <div>
-    <DateCounter />
+  return <div className='app'>
+    <Header />
+    <Main>
+      <p>1/15</p>
+      <p>Questions</p>
+    </Main>
   </div>;
 }
 
